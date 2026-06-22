5b7849795d70a162688c0b15a7c28e0031637602
Neelraj D Neharkar <neharkarneelraj@gmail.com>
2026-06-22 22:25:06 +0530

    Updated Random_Joke_Generator

--- Random_Joke_Generator/main.py
+++ Random_Joke_Generator/main.py
@@ -5,14 +5,34 @@ print("RANDOM JOKE GENERATOR")
 print("=========================")
 
 url="https://official-joke-api.appspot.com/random_joke"
-response=requests.get(url)
 
-if(response.status_code==200):
-    data=response.json()
+while True:
+    response = requests.get(url)
 
-    print(data["setup"])
-    print(data["punchline"])
-else:
-    print("Failed to fetch joke.")
-    
+    if response.status_code == 200:
+        data = response.json()
 
+        print("\nJoke:")
+        print(data["setup"])
+
+        input("\nPress Enter to reveal the punchline:)")
+
+        print(data["punchline"])
+
+    else:
+        print("Failed to fetch joke.")
+        break
+
+    while True:
+        another_joke = input("\nDo you want another joke? (Y/N):) ")
+
+        if another_joke.lower() == "y":
+            print("\n-----------------------------")
+            break
+
+        elif another_joke.lower() == "n":
+            print("\nThank you for using Random Joke Generator!")
+            exit()
+
+        else:
+            print("Invalid input! Please enter Y or N.")
